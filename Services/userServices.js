ed78e4fd1e8eb705de019b4b0704e2b72ed300d4
신유진 <julia011097@gmail.com>
2023-09-12 20:06:38 +0900

    회원가가입 로그인 토큰

--- Services/userServices.js
+++ Services/userServices.js
@@ -1,63 +1,130 @@
-//회원가입
 const signUp = async (req, res) => {
-    try {
-      const me = req.body;
-      console.log(me);
-  
-      // 2. DATABASE 정보 저장.
-      const { password, email, nickname } = me;
-  
-      // email, password가 다 입력되지 않은 경우
-      if (email === undefined || password === undefined || nickname === undefined) {
-        const error = new Error("KEY_ERROR");
-        error.statusCode = 400;
-        throw error;
-      }
+  try {
+    const me = req.body;
+    console.log(me);
 
-      // 이메일 중복 
-      const existingUser = await myDataSource.query(`
+    const { password, email, nickname } = me;
+
+    if (
+      email === undefined ||
+      password === undefined ||
+      nickname === undefined
+    ) {
+      const error = new Error("KEY_ERROR");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    const existingUser = await myDataSource.query(`
       SELECT id, email FROM users WHERE email='${email}';   
       `);
-      console.log("existing user:", existingUser);
+    console.log("existing user:", existingUser);
 
-      if (existingUser.length > 0) {
-        const error = new Error("DUPLICATED_EMAIL_ADDRESS");
-        error.statusCode = 400;
-        throw error;
-      }
-// email 특수문자 (./@ 포함)
-      const emailRegex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g;
-      const extractedEmails = email.match(emailRegex);
-      if(!email.match(emailRegex)) {
-        const error = new Error("특수문자 사용 안함")
-        error.statusCode = 400
-        throw error
-      }
-        console.log(extractedEmails);
-  
+    if (existingUser.length > 0) {
+      const error = new Error("DUPLICATED_EMAIL_ADDRESS");
+      error.statusCode = 400;
+      throw error;
+    }
 
-//패스워드 10자리 이상
-            if (password.length < 10) {
-        const error = new Error("INVALID_PASSWORD");
-        error.statusCode = 400;
-        throw error;
-      }
+    const emailRegex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g;
+    const extractedEmails = email.match(emailRegex);
+    if (!email.match(emailRegex)) {
+      const error = new Error("특수문자 사용 안함");
+      error.statusCode = 400;
+      throw error;
+    }
+    console.log(extractedEmails);
 
-   // 비밀번호 해쉬화
-  // const hashedPassword = await bcrypt.hash(password, 10)
-      
+    if (password.length < 10) {
+      const error = new Error("INVALID_PASSWORD");
+      error.statusCode = 400;
+      throw error;
+    }
 
-
-//DB에 저장 
     const userData = await myDataSource.query(`
-        INSERT INTO users (
-        name,                    
+        INSERT INTO users (                    
         password,
-        email, nickname
+        email, 
+        nickname
         )
         VALUES (
-        'yj',
+        '${nickname}'
         '${password}', 
         '${email}'
         )
     `);
+
+    console.log("after insert into", userData);
+
+    return res.status(201).json({
+      message: "userCreated",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
+};
+
+const logIn = async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if (email === undefined || password === undefined) {
+      const error = new Error("KEY_ERROR");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    const existingUser = await myDataSource.query(`
+    SELECT id, email, password FROM users WHERE email='${email}';
+    `);
+    console.log("existing user:", existingUser);
+
+    if (existingUser.length === 0) {
+      const error = new Error("DUPLICATED_EMAIL_ADDRESS");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    console.log("existing user:", existingUser);
+    console.log("email", "password");
+
+    console.log(password);
+
+    if (password !== existingUser[0].password) {
+      const error = new Error("INVALID_PASSWORD");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    const token = jwt.sign({ id: existingUser[0].id }, "rekey");
+    return res.status(200).json({
+      message: "LOGIN_SUCCESS",
+      accessToken: token,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getUsers = async (req, res) => {
+  try {
+    const userData = await myDataSource.query(
+      "SELECT id, nickname, email FROM USERS "
+    );
+
+    console.log("USER DATA:", userData);
+
+    return res.status(200).json({
+      users: userData,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
