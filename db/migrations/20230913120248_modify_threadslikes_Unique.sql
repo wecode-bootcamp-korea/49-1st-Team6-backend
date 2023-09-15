2dcbcba8a7c1e8d7ef7305781a8ffb3101c02b9e
신유진 <julia011097@gmail.com>
2023-09-15 09:37:48 +0900

    app.js user post env envsample 모두 수정, bcrpyt 설치, 해쉬 + typeorm jwt 추가 + dmbate migration modify

--- db/migrations/20230913120248_modify_threadslikes_Unique.sql
+++ db/migrations/20230913120248_modify_threadslikes_Unique.sql
@@ -1,5 +1,5 @@
 -- migrate:up
-ALTER TABLE thread_likes ADD CONSTRAINT UNIQUE unique_user_thread (user_id, thread_id)
+ALTER TABLE thread_likes ADD CONSTRAINT unique_user_thread  UNIQUE (user_id, thread_id)
 
 -- migrate:down
 
